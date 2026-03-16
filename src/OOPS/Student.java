72aa4f68ae73bc30a8c8a7b059c219662c987685
Nikshay Mohla <nikshay.mohla@rx.local>
2026-03-16 11:04:01 +0530

    constructors 1

--- src/OOPS/Student.java
+++ src/OOPS/Student.java
@@ -5,9 +5,9 @@ public class Student {
     String sname;
     char grade;
 
-//    void printData() {
-//        System.out.println(sid + "\nStudent Name:" + sname + "\nGrade:" + grade);
-//    }
+    void printData() {
+        System.out.println(sid + "\nStudent Name:" + sname + "\nGrade:" + grade);
+    }
 
     //Setter
     void setStudentData(int id, String name, char grade1) {
@@ -21,4 +21,30 @@ public class Student {
     void getStudentData() {
         System.out.println(sid + "\nStudent Name:" + sname + "\nGrade:" + grade);
     }
+
+
+//    constructor has no return value because no logic
+//    Automatically invokes when object is created
+//    can write multiple constructors but need to change parameters
+//    Only for assignment values no operations initializing data
+
+    Student(int id, String name, char grade1){
+        sid=id;
+        sname=name;
+        grade=grade1;
+    }
+
+//    difference bw constructors and methods
+//    method can be named anything constructors name should be the same as the class name
+//    Constructor will never return any values
+//    No Need to specify void in constructors
+//    Constructor automatically invoked during object creation
+//    Used for initializing the values of the variables
+//    methods used to specifying logic
+//    method can take parameters/arguments
+//    method may or maynot return a value
+
+
+//
+
 }
--- src/OOPS/StudentMain.java
+++ src/OOPS/StudentMain.java
@@ -3,19 +3,28 @@ package OOPS;
 public class StudentMain {
 
     public static void main(String[] args) {
-        Student stu1 = new Student();
+        Student stu1 = new Student(1, "Nikshay", 'A');
+//        First
 //        stu1.sname = "Nikshay";
 //        stu1.sid = 1;
 //        stu1.grade = 'A';
-        stu1.setStudentData(1,"Nikshay",'A');
-        stu1.getStudentData();
+//        Second using getters and setters
+//        stu1.setStudentData(1, "Nikshay", 'A');
+//        stu1.getStudentData();
+//        Third using constructor
+        stu1.printData();
 
-        Student stu2 = new Student();
+
+        Student stu2 = new Student(2, "Abhishek", 'B');
+//        First
 //        stu2.sname = "Abhishek";
 //        stu2.sid = 2;
 //        stu2.grade = 'B';
-        stu1.setStudentData(2,"Abhishek",'B');
-        stu2.getStudentData();
+//        Second using getters and setters
+//        stu1.setStudentData(2, "Abhishek", 'B');
+//        stu2.getStudentData();
+//        Third using constructor
+        stu2.printData();
     }
 
 }
